123223fe996cb0da4d9de27d249e7074c1581370
ziyad-bit <ziyad199523@gmail.com>
2025-06-16 23:58:18 +0300

    lang

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Button from "../components/Button";
 import Card from "../components/Card";
 import LabelInput from "../components/LabelInput";
@@ -10,13 +10,37 @@ import { useTranslation } from "react-i18next";
 export default function Login() {
     const navigate = useNavigate();
     const [inputs, setInputs] = useState({ email: "", password: "" });
+    const [lang, setLang] = useState('');
     const { handleMessageState } = useContext(AlertContext);
     const { t, i18n } = useTranslation();
 
+    useEffect(() => {
+        const lang = localStorage.getItem("i18nextLng");
+        if (lang == "ar") {
+            document.querySelector("html").dir = "rtl";
+            setLang('ar')
+        }
+    }, []);
+
     const handleLang = () => {
         i18n.changeLanguage("ar");
-        document.querySelector('html').dir='rtl'
+        document.querySelector("html").dir = "rtl";
+        setLang('ar')
     };
+
+    const handleLangEn = () => {
+        i18n.changeLanguage("en");
+        document.querySelector("html").dir = "ltr";
+        setLang('en')
+    };
+
+    const handleLangFr = () => {
+        i18n.changeLanguage("fr");
+        document.querySelector("html").dir = "ltr";
+        setLang('fr')
+    };
+
+    const rtl = lang == 'ar' ? true : false;
 
     const handleChange = (value, name) => {
         setInputs({ ...inputs, [name]: value });
@@ -49,11 +73,23 @@ export default function Login() {
     return (
         <>
             <Button
-                classes={"bg-green-700  hover:bg-green-800"}
+                classes={`bg-green-700 ${rtl ? 'mt-5' : null}  hover:bg-green-800`}
                 text={"lang"}
                 handleClick={handleLang}
             />
-            <div className="flex justify-center items-center h-100 mt-7">
+
+            <Button
+                classes={"bg-green-700  hover:bg-green-800"}
+                text={"langEn"}
+                handleClick={handleLangEn}
+            />
+
+            <Button
+                classes={"bg-green-700  hover:bg-green-800"}
+                text={"langFr"}
+                handleClick={handleLangFr}
+            />
+            <div className="ar flex justify-center items-center h-100 mt-7">
                 <Card
                     classes={"bg-blue-900 w-100"}
                     title={t("login")}
